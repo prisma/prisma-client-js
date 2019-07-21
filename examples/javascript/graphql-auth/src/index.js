2e1cb51f858fc0e42bc3000917d87734eb4d4d2b
gihrig <glen@arkadias.com>
2019-07-20 20:55:26 -0700

    enable graphql-shield in JS graphql-auth example

--- examples/javascript/graphql-auth/src/index.js
+++ examples/javascript/graphql-auth/src/index.js
@@ -9,7 +9,10 @@ const photon = new Photon.default()
 const server = new GraphQLServer({
   typeDefs: 'src/schema.graphql',
   resolvers,
-  // middlewares: [permissions], TODO: Fix after https://github.com/maticzav/graphql-shield/issues/361
+  // TODO: requires graphql-middleware 3.0.1
+  // 3.0.2 throws TypeError: definitions.trim is not a function
+  // see https://github.com/prisma/graphql-middleware/issues/198
+  middlewares: [permissions],
   context: request => {
     return {
       ...request,
